38630f79361d19417c50f113383e8c46b0f5e824
onkar kshirsagar <166027864+onkar-2006@users.noreply.github.com>
2025-01-29 18:18:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+import os 
+groq_api_key=os.getenv("groq_api_key")
+os.environ["groq_api_key"]=groq_api_key
+
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate 
+from langchain_core.output_parsers import StrOutputParser
+
+template=ChatPromptTemplate.from_messages(
+    [
+        ("system" ,"u are the expert in the quetion and the answering give the answer of the ask qution properly"),
+        ("user" ,"{input}")
+    ]
+)
+
+model=ChatGroq(api_key=groq_api_key)
+output=StrOutputParser()
+
+chain=template|model|output
+
+st.title("Simple GenAI App")
+input_=st.text_input("ask anything which is in your mind")
+
+if input_:
+    responce=chain.invoke({"input":input_})
+    st.write(responce)
+
+
+    
+
+
+
+
